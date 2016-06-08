3f8c5fccd076d3c6c122f2076ffce5468760f292
Sam Teale <sam.teale@ttysteale.co.uk>
2016-06-08 14:37:35 +0100

    Remove unwanted logging, leave the caller to decide on this not the library

--- registry/catalog.go
+++ registry/catalog.go
@@ -6,7 +6,6 @@ type catalogResponse struct {
 
 func (registry *Registry) Catalog() ([]string, error) {
 	url := registry.url("/v2/_catalog")
-	registry.Logf("registry.catalog url=%s", url)
 
 	var response catalogResponse
 	if err := registry.getJson(url, &response); err != nil {
--- registry/tags.go
+++ registry/tags.go
@@ -6,7 +6,6 @@ type tagsResponse struct {
 
 func (registry *Registry) Tags(repository string) ([]string, error) {
 	url := registry.url("/v2/%s/tags/list", repository)
-	registry.Logf("registry.tags url=%s repository=%s", url, repository)
 
 	var response tagsResponse
 	if err := registry.getJson(url, &response); err != nil {
